8d889e2aebf2d2e4fcfdd550b22f1aa5a9341191
Yanzu-zz <2273359360@qq.com>
2021-04-20 19:56:01 +0800

    The recoded of BST

---
+++ BinarySearchTree/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        BSTree<Integer> bst = new BSTree<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for (int num : nums)
+            bst.add(num);
+
+        bst.preOrder();
+        System.out.println();
+        System.out.println(bst);
+    }
+}
